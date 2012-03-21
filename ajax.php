618ec8c5fd6e8f4a383e0eea7353befdbf8547f6
Jesper Wøldiche <jesper@woeldiche.dk>
2012-03-21 14:25:12 +0100

    initial commit

---
+++ ajax.php
@@ -0,0 +1,3 @@
+<h1>Fantastisk titel</h1>
+<p>Og noget indhold. Sådan rigtigt med tekst og det hele.</p>
+<img src="http://placehold.it/350x150">
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>   
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<!--[if IE]><![endif]-->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>--- A ---</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="" />
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	<!-- !CSS -->
+	<link rel="stylesheet" href="css/style.a.css?v=1">
+	<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  
+  <!-- FFF Widget Code - So far does nothing :-) -->
+	<script type="text/javascript">
+
+    var fffWidgetConfig = [];
+    fffWidgetConfig.push({
+      "widget" : "interactive",
+      "target" : "#widget1",
+      "style"  : "full"
+    });
+    fffWidgetConfig.push({
+      "widget" : "slidein",
+      "target" : "#widget2",
+      "style"  : "bare"
+    });
+  
+    (function() {
+      var fff = document.createElement('script'); fff.type = 'text/javascript'; fff.async = true;
+      fff.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'service.finurligefakta.dk/fff.widget.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(fff, s);
+    })();
+  
+  </script>
+</head>
+<!-- !Body -->
+<body>
+	<div class="container">
+		<header>
+			<ul class="nav">
+				<li><a href="#loadlocal" title="Home">Load local</a></li>
+			</ul><!-- /#nav  -->
+		</header><!-- /header -->
+		
+		<section class="content">
+		
+		</section><!-- /main -->
+		  <p>Content before widget</p>
+		  <div id="widget1"></div>
+		  <p>Content after widget</p>
+		<footer>
+		
+		</footer><!-- /footer -->
+	</div><!--!/#container -->
+	<!-- !Javascript - at the bottom for fast page loading -->
+
+	
+</body>
+</html>
